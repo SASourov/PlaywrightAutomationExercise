7adf4da48d9f745b7954099b5ce7946439d1eff2
shakil.ahmed <shakil.ahmed@amiprobashi.com>
2025-06-30 14:07:37 +0600

    Add ne Feature - search product add to cart product

--- Pages/AddToCartPage.js
+++ Pages/AddToCartPage.js
@@ -2,6 +2,9 @@ exports.AddToCartPage = class AddToCart{
     constructor (page){
         this.page = page
 
+        this.productMenu = page.locator("//a[@href='/products']")
+        this.searchBox = page.locator("//input[@id='search_product']")
+        this.searchBtn = page.locator("//i[@class='fa fa-search']")
         this.viewProduct = page.locator("//div[@class='col-sm-9 padding-right']//div[2]//div[1]//div[2]//ul[1]//li[1]//a[1]")
         this.productQuantity = page.locator('//*[@id="quantity"]')
         this.addToCrtBtn = page.locator("//button[normalize-space()='Add to cart']")
@@ -9,6 +12,15 @@ exports.AddToCartPage = class AddToCart{
     }
 
     async adddToCartProduct(){
+        await this.productMenu.click()
+        await this.page.waitForTimeout(1000)
+
+        await this.searchBox.fill("Tshirt")
+        await this.page.waitForTimeout(1000)
+
+        await this.searchBtn.click()
+        await this.page.waitForTimeout(1000)
+
         await this.viewProduct.click()
         await this.page.waitForTimeout(1000); 
 
